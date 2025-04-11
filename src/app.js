29c74aa1308e87e5e0a1d744d960de3177341d17
Ferbarotz <ferbarotz23@gmail.com>
2025-04-11 20:09:15 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['Mi padre', 'Mi madre', 'Mi hermano', 'Mi abuelo'];
+  let action = ['rompio', 'perdio', 'olvido', 'refundio'];
+  let what = ['el celular', 'el reloj', 'las llaves', 'la tarjeta'];
+  let when = ['en la mañana', 'en la noche', 'en la tarde', 'ayer'];
+
+function generadorDeExcusas(who, action, what,  when) {
+  
+  let randomwho = Math.floor(Math.random() * who.length);
+  let randomaction = Math.floor(Math.random() * action.length);
+  let randomwhat = Math.floor(Math.random() * what.length);
+  let randomwhen = Math.floor(Math.random() * when.length);
+return (who[randomwho] + " " + action[randomaction]+ " "  + what[randomwhat]+ " "  +  when[randomwhen]);
+
+}
+
+  console.log(generadorDeExcusas(who, action, what,  when));
+
+  document.querySelector("#excuse").innerHTML=generadorDeExcusas(who, action, what,  when);
 };
+
+  
